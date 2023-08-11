7ec036f2aece38bebffca4ddaecc363d2b63e049
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-08-11 01:01:36 -0300

    update CSV system

--- src/server/controllers/Order/getCSV.ts
+++ src/server/controllers/Order/getCSV.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
+// import { StatusCodes } from "http-status-codes";
 import { OrderProvider } from "../../database/providers/Order";
 import { validation } from "../../shared/middleware";
+import { createObjectCsvWriter } from "csv-writer";
 import * as yup from "yup";
+import * as path from "path";
 
 interface IQueryProps {
     created_at?: Date;
@@ -16,17 +18,41 @@ export const getCSVValidation = validation((getSchema) => ({
     })), 
 }));
 
-export const getCSV = async (req: Request<{}, {}, {}, IQueryProps>, res: Response): Promise<Response> => {
-
+export const getCSV = async (req: Request<{}, {}, {}, IQueryProps>, res: Response): Promise<void> => {
     const user_id = Number(req.headers.user_id);
 
-    // Chama o provider (provedor) para buscar todos os pedidos com base nos parâmetros passados na query.
-    // Se os parâmetros não forem fornecidos, usa-se valores padrão: página 1, limite 7, data mínima e preço total 0.
-    const result = await OrderProvider.getCSV(
-        user_id, 
-        req.query.created_at ? new Date(req.query.created_at) : new Date(0), 
+    const data = await OrderProvider.getCSV(
+        user_id,
+        req.query.created_at ? new Date(req.query.created_at) : new Date(0),
         req.query.total_price || 0
     );
 
-    return res.status(StatusCodes.OK).json(result);
+    const csvFilePath = path.join(__dirname, "relatorio_compras.csv");
+
+    res.setHeader("Content-Disposition", "attachment; filename=relatorio_compras.csv");
+    res.setHeader("Content-Type", "text/csv");
+
+    const csvWriter = createObjectCsvWriter({
+        path: csvFilePath,
+        header: [
+            { id: "order_id", title: "Order ID" },
+            { id: "total_price", title: "Total Price" },
+            { id: "created_at", title: "Created At" },
+            { id: "order_items", title: "Order Items" },
+        ],
+    });
+
+    const records = data.map(order => {
+        const orderItemsText = order.order_items.map(item => `${item.quantity} ${item.name}`).join(", ");
+        return {
+            order_id: order.order_id,
+            total_price: order.total_price,
+            created_at: order.created_at,
+            order_items: orderItemsText,
+        };
+    });
+
+    await csvWriter.writeRecords(records);
+
+    return res.sendFile(csvFilePath);
 };
