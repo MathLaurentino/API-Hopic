30c4e6171168a876283898e7024ce897e6720e50
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-08-20 16:23:30 -0300

    Update Digital Ocean 2

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,107 +1,32 @@
-import { Router } from 'express'
-import {
-  UsuarioController,
-  ProdutoController,
-  OrderController,
-  OrderItemController
-} from './../controllers'
-import {
-  uploadImage,
-  sanitizeInput,
-  ensureAuthenticated
-} from '../shared/middleware'
+import { Router } from "express";
+import { OrderController, OrderItemController} from "./../controllers";
+import { UsuarioController } from "../controllers/Usuario";
+import { ProdutoController } from "../controllers/Produto";
+import { uploadImage, sanitizeInput, ensureAuthenticated } from "../shared/middleware";
 
-const router = Router()
+const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send('Loja ligado!')
-})
+router.get("/", (req, res) => {
+    return res.send("Loja ligado!");
+});
 
-router.post(
-  '/signup',
-  UsuarioController.signUpValidation,
-  UsuarioController.signUp
-)
-router.post(
-  '/signin',
-  UsuarioController.signInValidation,
-  UsuarioController.signIn
-)
-router.post(
-  '/passwordReset',
-  UsuarioController.passwordResetValidation,
-  UsuarioController.passwordResetRequest
-)
-router.post(
-  '/newPassword/:chave',
-  UsuarioController.newPasswordValidation,
-  UsuarioController.newPasswordRequest
-)
-router.post(
-  '/resendEmailConfirmation',
-  UsuarioController.resendEmailConfirmationValidation,
-  UsuarioController.resendEmailConfirmation
-)
-router.get(
-  '/validateEmail/:chave',
-  UsuarioController.validateEmailValidation,
-  UsuarioController.validateEmail
-)
+router.post("/signup", UsuarioController.signUpValidation, UsuarioController.signUp);
+router.post("/signin", UsuarioController.signInValidation, UsuarioController.signIn);
+router.post("/passwordReset", UsuarioController.passwordResetValidation, UsuarioController.passwordResetRequest);
+router.post("/newPassword/:chave", UsuarioController.newPasswordValidation, UsuarioController.newPasswordRequest);
+router.post("/resendEmailConfirmation", UsuarioController.resendEmailConfirmationValidation, UsuarioController.resendEmailConfirmation);
+router.get("/validateEmail/:chave", UsuarioController.validateEmailValidation, UsuarioController.validateEmail);
 
-router.put(
-  '/produtos/:id',
-  ensureAuthenticated,
-  uploadImage.single('image'),
-  sanitizeInput,
-  ProdutoController.updateByIdValidation,
-  ProdutoController.updateById
-)
-router.post(
-  '/produtos',
-  ensureAuthenticated,
-  uploadImage.single('image'),
-  sanitizeInput,
-  ProdutoController.createValidation,
-  ProdutoController.create
-)
-router.delete(
-  '/produtos/:id',
-  ensureAuthenticated,
-  ProdutoController.deleteByIdValidation,
-  ProdutoController.deleteById
-)
-router.get(
-  '/produtos/:id',
-  ensureAuthenticated,
-  ProdutoController.getByIdValidation,
-  ProdutoController.getById
-)
-router.get('/produtos', ensureAuthenticated, ProdutoController.getAll)
+router.put("/produtos/:id", ensureAuthenticated, uploadImage.single("image"), sanitizeInput, ProdutoController.updateByIdValidation, ProdutoController.updateById);
+router.post("/produtos", ensureAuthenticated, uploadImage.single("image"), sanitizeInput, ProdutoController.createValidation, ProdutoController.create);
+router.delete("/produtos/:id", ensureAuthenticated, ProdutoController.deleteByIdValidation, ProdutoController.deleteById);
+router.get("/produtos/:id", ensureAuthenticated, ProdutoController.getByIdValidation, ProdutoController.getById);
+router.get("/produtos", ensureAuthenticated, ProdutoController.getAll);
 
-router.post(
-  '/Order',
-  ensureAuthenticated,
-  OrderController.createValidation,
-  OrderController.create
-)
-router.get(
-  '/Order',
-  ensureAuthenticated,
-  OrderController.getAllValidation,
-  OrderController.getAll
-)
-router.get(
-  '/getXLSX',
-  ensureAuthenticated,
-  OrderController.getXLSXValidation,
-  OrderController.getXLSX
-)
+router.post("/Order", ensureAuthenticated, OrderController.createValidation, OrderController.create);
+router.get("/Order", ensureAuthenticated, OrderController.getAllValidation, OrderController.getAll);
+router.get("/getXLSX", ensureAuthenticated, OrderController.getXLSXValidation, OrderController.getXLSX);
 
-router.get(
-  '/OrderItem/:order_id',
-  ensureAuthenticated,
-  OrderItemController.getByOrderIdValidation,
-  OrderItemController.getByOrderId
-)
+router.get("/OrderItem/:order_id", ensureAuthenticated, OrderItemController.getByOrderIdValidation, OrderItemController.getByOrderId);
 
-export { router }
+export { router };
