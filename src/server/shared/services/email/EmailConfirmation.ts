503c715aeee427e69f4b6fc6f04f68c2d4e82790
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-09-02 22:04:39 -0300

    Update Email Service
    Melhorei o body dos email de confirmação de cadastro e de redefinição de senha, alem de adicionar o logger.warn caso tenha falha ao enviar o email ao usuario

--- src/server/shared/services/email/EmailConfirmation.ts
+++ src/server/shared/services/email/EmailConfirmation.ts
@@ -1,5 +1,6 @@
 import * as nodemailer from "nodemailer";
 import "dotenv/config";
+import logger from "../logger";
 
 export const EmailConfirmation = (nome: string, email: string, uniqueString: string) => {
 
@@ -12,18 +13,51 @@ export const EmailConfirmation = (nome: string, email: string, uniqueString: str
         }
     });
 
+    const confirmacaoLink = process.env.NODE_ENV === "production" ? 
+        `https://hopic.store/validateEmail/${uniqueString}` : 
+        `http://localhost:${process.env.PORT}/validateEmail/${uniqueString}`;
+
     const mailOptions = {
-        from: "matheuscalifornia29@gmail.com", // sender address
-        to: email, // list of receivers
-        subject: "Confirmação de E-Mail", // Subject line
-        text: "Olá, prezado(a) " + nome + ". Pressione <a href='http://localhost:3000/validateEmail/" + uniqueString + "'> Aqui </a> para ativar sua conta",
-        html: "Olá, prezado(a) " + nome + ". Pressione <a href='http://localhost:3000/validateEmail/" + uniqueString + "'> Aqui </a> para ativar sua conta",
+        from: "hopic.store@gmail.com",
+        to: email,
+        subject: "Confirmação de Cadastro - hopic.store",
+        text: `Olá ${nome},\n\nObrigado por se cadastrar na hopic.store! Clique no link a seguir para confirmar seu cadastro:
+                \n${confirmacaoLink}\n\nSe você não se cadastrou em nosso site, por favor, ignore este e-mail.\n`,
+        html: corpoHTML(nome, confirmacaoLink),
     };
 
     transport.sendMail(mailOptions, function(err){
         if (err) {
-            console.log("Erro ao enviar Email: " + err );
+            logger.warn("Falha ao enviar e-mail de confirmação ao e-mail " + email + "\n" + err);
         }
     });
 
+};
+
+const corpoHTML = (nome: string, confirmacaoLink: string): string => {
+    return `
+        <!DOCTYPE html>
+        <html lang="pt-BR">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>EConfirmação de Cadastro</title>
+        </head>
+        <body style="background-color: #f2f2f2; margin: 0; padding: 0; font-family: Arial, sans-serif;">
+            <table role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0">
+                <tr>
+                    <td style="text-align: center;">
+                        <div style="padding: 20px;">
+                            <h1 style="color: #333;">Confirmação de Cadastro - hopic.store</h1>
+                            <p style="color: #333;">Olá ${nome},</p>
+                            <p>Obrigado por se cadastrar na hopic.store! Clique no link abaixo para confirmar seu cadastro:</p>
+                            <p><a href="${confirmacaoLink}" style="text-decoration: none; background-color: #007bff; color: #fff; padding: 10px 20px; border-radius: 5px;">Confirmar Cadastro</a></p>
+                            <p>Se você não se cadastrou em nosso site, por favor, ignore este e-mail.</p>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </body>
+        </html>
+    `;
 };
--- src/server/shared/services/email/EmailNewPassWord.ts
+++ src/server/shared/services/email/EmailNewPassWord.ts
@@ -1,5 +1,6 @@
 import * as nodemailer from "nodemailer";
 import "dotenv/config";
+import logger from "../logger";
 
 export const EmailNewPassWord = (nome: string, email: string, uniqueString: string) => {
 
@@ -12,18 +13,57 @@ export const EmailNewPassWord = (nome: string, email: string, uniqueString: stri
         }
     });
 
+
+    const linkRedefinicao = process.env.NODE_ENV === "production" ? 
+        `https://hopic.store/newPassword/${uniqueString}` : 
+        `http://localhost:${process.env.PORT}/newPassword/${uniqueString}`;
+
+    
     const mailOptions = {
         from: "matheuscalifornia29@gmail.com", // sender address
         to: email, // list of receivers
         subject: "Confirmação de E-Mail", // Subject line
-        text: "Olá, prezado(a) " + nome + ". Pressione <a href='http://localhost:3000/newPassword/" + uniqueString + "'> Aqui </a> para ser redirecionado a página de alterar senha",
-        html: "Olá, prezado(a) " + nome + ". Pressione <a href='http://localhost:3000/newPassword/" + uniqueString + "'> Aqui </a> para ser redirecionado a página de alterar senha",
+        text: `Olá ${nome}, Recebemos uma solicitação para redefinir a senha da sua conta na hopic.store. 
+            Clique no link abaixo para continuar o processo de redefinição de senha: <a href='${linkRedefinicao}'>
+            Se você não solicitou a redefinição de senha, pode ignorar este e-mail. Atenciosamente,Equipe hopic.store`,
+        html: corpoHTML(nome, linkRedefinicao)
     };
 
     transport.sendMail(mailOptions, function(err){
         if (err) {
-            console.log("Erro ao enviar Email: " + err );
+            logger.warn("Falha ao enviar e-mail de redefinição de senha ao e-mail " + email + "\n" + err);
         }
     });
 
 };
+
+
+const corpoHTML = (nome: string, linkRedefinicao: string): string => {
+    return `
+        <!DOCTYPE html>
+        <html lang="pt-BR">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Redefinição de Senha - hopic.store</title>
+        </head>
+        <body style="background-color: #f2f2f2; margin: 0; padding: 0; font-family: Arial, sans-serif;">
+            <table role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0">
+                <tr>
+                    <td style="text-align: center;">
+                        <div style="padding: 20px;">
+                            <h1 style="color: #333;">Redefinição de Senha - hopic.store</h1>
+                            <p>Olá ${nome},</p>
+                            <p>Recebemos uma solicitação para redefinir a senha da sua conta na hopic.store. Clique no link abaixo para continuar o processo de redefinição de senha:</p>
+                            <p><a href="${linkRedefinicao}" style="text-decoration: none; background-color: #007bff; color: #fff; padding: 10px 20px; border-radius: 5px;">Redefinir Senha</a></p>
+                            <p>Se você não solicitou a redefinição de senha, pode ignorar este e-mail.</p>
+                            <p>Atenciosamente,<br>Equipe hopic.store</p>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </body>
+        </html>
+    `;
+};
+
